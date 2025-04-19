237e5dd623ce132e792a9df088b8c944b35b95ea
wendiee03 <wmukami03@gmail.com>
2025-04-19 18:16:49 +0300

    setup react app and install react bootstrap UI

---
+++ react-jwt/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <Button>Hello</Button>
+  );
+}
+
+export default App;
